3aa0e34f139e62ff8e52f444503ffeb1dd607c68
Allanah Malate <allanahjonas25@gmail.com>
2025-10-21 14:54:27 +0200

    correccao (minha)

--- T2_25/src/EX1.java
+++ T2_25/src/EX1.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.text.*;
-import java.util.*;
+//import java.util.*;
 
 
 public class EX1 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
         byte linhas=3, colunas=4, livres;
         float[][] cofre = new float[linhas][colunas];
@@ -33,64 +33,84 @@ public class EX1
             return cont;
     }
         
-        public static byte[] procValorMaior(float[][] array)
-        {
-            byte indiceL=0,indiceC=0;
-            float maior;
-            
-            
-        for(int i=0; i<array.length; i++)
+    public static byte[] procValorMaior(float[][] array)
+    {
+        byte indiceL=0,indiceC=0;
+        float maior;
+        
+        for(byte i=0; i<array.length; i++)
         { 
             maior = array[i][0];
-            for(int j=1; j<array[0].length; j++)
+            for(byte j=1; j<array[0].length; j++)
                 if(array[i][j] > maior)
                 {
                     maior = array[i][j];
-                    indiceL = i;
-                    indiceC = j;
+                    indiceL = i;  //ERROR! Type mismatch: cannot convert from int to byte
+                    indiceC = j;  //ERROR! Type mismatch: cannot convert from int to byte
                 }
-            }
-            
-            byte[] indice = {indiceL,indiceC};
-            return indice;
         }
         
-        public static double calcTotGlobal(float[][] array)
-        {
-            double total=0;
+        byte[] indice = {indiceL,indiceC};
+        return indice;
+    }
     
-            for(int i=0; i<array.length; i++)
-                for(int j=0; j<array[0].length; j++)
-                total += array[i][j];
-                
-                return total;
-        }
+    public static double calcTotGlobal(float[][] array)
+    {
+        double total=0;
 
-        public static float[][] preencherArray(byte l, byte c)
-        {
-            float[][] array = new float[l][c];
+        for(int i=0; i<array.length; i++)
+            for(int j=0; j<array[0].length; j++)
+            total += array[i][j];
             
-            for(int i=0; i<array.length; i++)
-            {
-                System.out.println((i+1) + "o Nivel");
-                for(int j=0; j<array[0].length; j++)
-                    array[i][j] = validarFloat(j);  
-            }
-            return array;
-        }
+            return total;
+    }
 
-        public static void visualizarArray(float[][] array)
+    public static float[][] preencherArray(byte l, byte c) throws IOException
+    {
+        float[][] array = new float[l][c];
+        
+        for(int i=0; i<array.length; i++)
         {
-            
+            System.out.println((i+1) + "o Nivel");
+            for(byte j=0; j<array[0].length; j++)
+                array[i][j] = validarFloat(j);      //ERROR: The method validarFloat(byte) int the type EX1 is not applicable for the argument (int)
+        }
+        return array;
+    }
+
+    public static void visualizarArray(float[][] array)
+    {
+        
         for(int i=0; i<array.length; i++)
         {
             for(int j=0; j<array[0].length; j++)
-            {
                 System.out.println(array[i][j] + " ");
-            }
             System.out.println();
         }        
     }
 
-    
+    public static float validarFloat(byte i) throws IOException
+    {
+        float val;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        do
+        {
+            System.out.println("Valor da joalharia da " + (i+1) + "a coluna(>0): ");
+            val = Float.parseFloat(br.readLine());
+            if(val <= 0)
+                System.out.println("Erro!Tente Novamente.");
+        }while(val <= 0);
+
+        return val;
+    }
+
+    public static void visualizar(float[][] array, byte livres, double total, byte[] indice)
+    {
+        DecimalFormat mt = new DecimalFormat("###,###,###.00 MTS");
+        System.out.println("Existem " + livres + " posicoes do cofre que se encontram livres");
+        System.out.println("Valor total que existe no banco: " + mt.format(total));
+        System.out.println("O cliente com o maior valor monetario guardado encontra-se no \n" + indice[0] + "o Nivel\t" + indice[1] + "a coluna."); 
+    }
 }
